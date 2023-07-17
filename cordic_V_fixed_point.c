f6afe3d71bee5edde33d6abd2961d9442376dccd
Jack Klobchar <jackklobchar@gmail.com>
2023-07-16 22:58:33 -0700

    fix loop initialization

--- cordic_V_fixed_point.c
+++ cordic_V_fixed_point.c
@@ -1,20 +1,24 @@
 
 int z_table[15];
 
-void cordic_V_fixed_point( int *x, int *y, int *z) {
+void cordic_V_fixed_point(int *x, int *y, int *z)
+{
     int x_temp_1, y_temp_1, z_temp;
     int x_temp_2, y_temp_2;
     int i;
     x_temp_1 = *x;
     y_temp_1 = *y;
     z_temp = 0;
-    for( i=0; i<15; i++) { /* 15 iterations are needed */
-        if( y_temp_1 > 0) {
+    for (int i = 0; i < 15; i++)
+    { /* 15 iterations are needed */
+        if (y_temp_1 > 0)
+        {
             x_temp_2 = x_temp_1 + (y_temp_1 >> i);
             y_temp_2 = y_temp_1 - (x_temp_1 >> i);
             z_temp += z_table[i];
         }
-        else {
+        else
+        {
             x_temp_2 = x_temp_1 - (y_temp_1 >> i);
             y_temp_2 = y_temp_1 + (x_temp_1 >> i);
             z_temp -= z_table[i];
