08022ebb03f3fcf88da1a576db6b3e1a774d95f4
kevin <kmathew@peloton-technologies.com>
2023-07-24 00:44:29 -0700

    checking out perf tool

---
+++ executables/perf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create or clear the stats.txt file
+> stats.txt
+
+# Loop through all .exe files in the current directory and run "perf <filename>"
+for filename in *.exe; do
+    if [ -f "$filename" ]; then
+        echo "Running perf for file: $filename"
+        perf "$filename" >> stats.txt
+        echo "perf for file $filename completed."
+    fi
+done
+
+echo "All perf commands completed. Results are stored in stats.txt."
