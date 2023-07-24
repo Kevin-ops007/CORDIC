4e3861caccc2caa7dc6cf649c2de33f6f085e6cc
Jack Klobchar <jackklobchar@gmail.com>
2023-07-23 21:31:01 -0700

    fix bug loop unrolling

--- cordic_V_unrolled.c
+++ cordic_V_unrolled.c
@@ -5,11 +5,11 @@ void cordic_V_fixed_point(int *x, int *y, int *z)
 {
     int x_temp_1, y_temp_1, z_temp;
     int x_temp_2, y_temp_2;
-    int i;
+    int i = 0;
     x_temp_1 = *x;
     y_temp_1 = *y;
     z_temp = 0;
-    for (int i = 0; i < 14; i += 2)
+    for (; i < 14; i += 2)
     { /* 15 iterations are needed */
         if (y_temp_1 > 0)
         {
