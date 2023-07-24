994cca693aa9c841645856e4657e8c3b5fab05a6
Jack Klobchar <jackklobchar@gmail.com>
2023-07-23 21:27:09 -0700

    another change to unrolled

--- cordic_V_unrolled.c
+++ cordic_V_unrolled.c
@@ -28,21 +28,20 @@ void cordic_V_fixed_point(int *x, int *y, int *z)
 
         if (y_temp_1 > 0)
         {
-            x_temp_2 = x_temp_1 + (y_temp_1 >> i);
-            y_temp_2 = y_temp_1 - (x_temp_1 >> i);
-            z_temp += z_table[i];
+            x_temp_2 = x_temp_1 + (y_temp_1 >> i + 1);
+            y_temp_2 = y_temp_1 - (x_temp_1 >> i + 1);
+            z_temp += z_table[i + 1];
         }
         else
         {
-            x_temp_2 = x_temp_1 - (y_temp_1 >> i);
-            y_temp_2 = y_temp_1 + (x_temp_1 >> i);
-            z_temp -= z_table[i];
+            x_temp_2 = x_temp_1 - (y_temp_1 >> i + 1);
+            y_temp_2 = y_temp_1 + (x_temp_1 >> i + 1);
+            z_temp -= z_table[i + 1];
         }
         x_temp_1 = x_temp_2;
         y_temp_1 = y_temp_2;
     }
 
-    --i;
     if (y_temp_1 > 0)
     {
         x_temp_2 = x_temp_1 + (y_temp_1 >> i);
@@ -55,6 +54,7 @@ void cordic_V_fixed_point(int *x, int *y, int *z)
         y_temp_2 = y_temp_1 + (x_temp_1 >> i);
         z_temp -= z_table[i];
     }
+
     x_temp_1 = x_temp_2;
     y_temp_1 = y_temp_2;
     *x = x_temp_1;
