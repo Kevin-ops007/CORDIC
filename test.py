b769ca1085f294742831bf778cda5e5be7f75ea7
Jack Klobchar <jackklobchar@gmail.com>
2023-07-23 22:33:19 -0700

    redirect output in test

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import os
 def main():
     argv = sys.argv
     if(len(argv) != 3):
-        print(f'Must provide executable name. Usage: {argv[0]} executable_name num_iterations')
+        print(f'Must provide executable name. Usage: {argv[0]} ./executable_name num_iterations')
         exit(1)
 
     exe = argv[1]
@@ -16,13 +16,21 @@ def main():
     
     num_iterations = int(argv[2])
 
-    start_perf = time.perf_counter()
-    start_process = time.perf_counter()
-    for i in range(num_iterations):
-        subprocess.Popen([exe])
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        start_perf = time.perf_counter()
+        start_process = time.perf_counter()
+        
+        for i in range(num_iterations):
+            subprocess.Popen([exe])
+        end_perf = time.perf_counter()
+        end_process = time.process_time()
 
-    end_perf = time.perf_counter()
-    end_process = time.process_time()
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
 
     avg_perf_ms = end_perf / num_iterations * 1000      
     avg_proces_ms = end_process / num_iterations * 1000
