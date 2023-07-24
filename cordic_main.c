191cde34b812b10a84bb4a8a2cd525d8977655c2
kevin <kmathew@peloton-technologies.com>
2023-07-23 20:38:04 -0700

    adding floating point representation of results

--- cordic_main.c
+++ cordic_main.c
@@ -9,6 +9,9 @@ void verify( int x_i_init, int y_i_init, int z_i_init,int x_i, int y_i, int z_i)
     x_d_init = (double)x_i_init / ( 1 << 15); /* float image of x_*/
     y_d_init = (double)y_i_init / ( 1 << 15); /* float image of y_*/
     z_d_init = (double)z_i_init / ( 1 << 15); /* float image of z_*/
+    x_d = (double)x_i / ( 1 << 15); /* float image of x_i*/
+    y_d = (double)y_i / ( 1 << 15); /* float image of y_i*/
+    z_d = (double)z_i / ( 1 << 15); /* float image of z_i*/
     printf( "x_i_init = %5i\tx_d_init = %f\n", x_i_init, x_d_init);
     printf( "y_i_init = %5i\ty_d_init = %f\n", y_i_init, y_d_init);
     printf( "z_i_init = %5i\tz_d_init = %f (rad)\n\n", z_i_init, z_d_init);
