bd08e4be3c5ccf308c0c060e8f3f6f0b68a9bf78
kevin <kmathew@peloton-technologies.com>
2023-07-23 19:07:40 -0700

    unrolling

---
+++ cordic_V_fixed_point_unrolled.c
@@ -0,0 +1,62 @@
+
+int z_table[15];
+
+void cordic_V_fixed_point(int *x, int *y, int *z)
+{
+    int x_temp_1, y_temp_1, z_temp;
+    int x_temp_2, y_temp_2;
+    int i;
+    x_temp_1 = *x;
+    y_temp_1 = *y;
+    z_temp = 0;
+    for (int i = 0; i < 14; i+=2)
+    { /* 15 iterations are needed */
+        if (y_temp_1 > 0)
+        {
+            x_temp_2 = x_temp_1 + (y_temp_1 >> i);
+            y_temp_2 = y_temp_1 - (x_temp_1 >> i);
+            z_temp += z_table[i];
+        }
+        else
+        {
+            x_temp_2 = x_temp_1 - (y_temp_1 >> i);
+            y_temp_2 = y_temp_1 + (x_temp_1 >> i);
+            z_temp -= z_table[i];
+        }
+        x_temp_1 = x_temp_2;
+        y_temp_1 = y_temp_2;
+
+        if (y_temp_1 > 0)
+        {
+            x_temp_2 = x_temp_1 + (y_temp_1 >> i);
+            y_temp_2 = y_temp_1 - (x_temp_1 >> i);
+            z_temp += z_table[i];
+        }
+        else
+        {
+            x_temp_2 = x_temp_1 - (y_temp_1 >> i);
+            y_temp_2 = y_temp_1 + (x_temp_1 >> i);
+            z_temp -= z_table[i];
+        }
+        x_temp_1 = x_temp_2;
+        y_temp_1 = y_temp_2;
+    }
+
+    if (y_temp_1 > 0)
+    {
+        x_temp_2 = x_temp_1 + (y_temp_1 >> i);
+        y_temp_2 = y_temp_1 - (x_temp_1 >> i);
+        z_temp += z_table[i];
+    }
+    else
+    {
+        x_temp_2 = x_temp_1 - (y_temp_1 >> i);
+        y_temp_2 = y_temp_1 + (x_temp_1 >> i);
+        z_temp -= z_table[i];
+    }
+    x_temp_1 = x_temp_2;
+    y_temp_1 = y_temp_2;
+    *x = x_temp_1;
+    *y = y_temp_1;
+    *z = z_temp;
+}
