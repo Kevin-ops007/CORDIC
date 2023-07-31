b94540e50e1296c7e06fee865c46a54380e559ab
kevin <kmathew@peloton-technologies.com>
2023-07-31 14:07:53 -0700

    fixing some stuff

--- build.sh
+++ build.sh
@@ -12,21 +12,21 @@ for c_file in "${c_files[@]}"; do
     name=${name%.c}
     output_executable="cordic_$name.exe"
     arm_code="cordic_$name.s"
-    
-    echo "COMPILING $c_file"
 
     # Skip cordic_V_32bit.c
-    if [[ $c_file != "cordic_V_32bit.c" ]]; then
+    if [[ $c_file!="cordic_V_32bit.c" ]]; then
+        echo "COMPILING $c_file"
         # Compile the files with GCC
         gcc -O3 "$main_file" "$c_file" -o "$output_executable" -lm
         gcc -O3 -S "$c_file" -o "$arm_code"
-    elif [[ $c_file == "cordic_V_neon.c" ]]; then
+    elif [[ $c_file=="cordic_V_neon.c" ]]; then
         echo "SKIPPING NEON FOR NOW"
         # Do nothing until Neon is fixed
         # gcc -mfloat-abi=softfp -mfpu=neon -static -o "$output_executable" "$main_file" "$c_file" -lm   
         # gcc -mfloat-abi=softfp -mfpu=neon -static -S "$c_file" -o "arm_code"
     else
         # Generate the output executable name
+        echo "COMPILING 32bit $c_file"
         gcc -O3 cordic_main_32bit.c cordic_V_32bit.c -o cordic_32bit.exe -lm
         gcc -O3 -S "$c_file" -o "$arm_code"
         
