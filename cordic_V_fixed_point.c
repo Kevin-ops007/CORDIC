ce6489f76359d478e0a456b5444c028c33bb7d1b
Jack Klobchar <jackklobchar@gmail.com>
2023-07-30 21:41:22 -0700

    remove shifting and round

--- cordic_V_fixed_point.c
+++ cordic_V_fixed_point.c
@@ -6,8 +6,8 @@ void cordic_V_fixed_point(int *x, int *y, int *z)
     int x_temp_1, y_temp_1, z_temp;
     int x_temp_2, y_temp_2;
     int i;
-    x_temp_1 = (*x) << 2;
-    y_temp_1 = (*y) << 2;
+    x_temp_1 = *x;
+    y_temp_1 = *y;
     z_temp = 0;
     for (int i = 0; i < 15; i++)
     { /* 15 iterations are needed */
@@ -26,8 +26,7 @@ void cordic_V_fixed_point(int *x, int *y, int *z)
         x_temp_1 = x_temp_2;
         y_temp_1 = y_temp_2;
     }
-
-    *x = (x_temp_1 + 1) >> 2;
-    *y = (y_temp_1 + 1) >> 2;
+    *x = x_temp_1;
+    *y = y_temp_1;
     *z = z_temp;
 }
--- cordic_V_fp_12.c
+++ cordic_V_fp_12.c
@@ -9,8 +9,10 @@ void cordic_V_fixed_point(int *x, int *y, int *z)
 
     // take all variables as 12 bit samples
     // shift the samples to have 2 bit padding to left and 2 bit padding to right
-    x_temp_1 = (*x) << 2; // scale factor is now 2^13
-    y_temp_1 = (*y) << 2; // scale factor is now 2^13
+    // x_temp_1 = (*x) << 2; // scale factor is now 2^13
+    // y_temp_1 = (*y) << 2; // scale factor is now 2^13
+    x_temp_1 = *x;
+    y_temp_1 = *y;
     z_temp = 0;
 
     for (int i = 0; i < 11; i++)
@@ -36,8 +38,8 @@ void cordic_V_fixed_point(int *x, int *y, int *z)
     // 1243 may be more precise
     x_temp_1 *= 1244;
 
-    x_temp_1 = (x_temp_1 + 1) >> 2; // scale factor back to 2^11
-    y_temp_1 = (y_temp_1 + 1) >> 2; // scale factor back to 2^11
+    // x_temp_1 = (x_temp_1 + 1) >> 2; // scale factor back to 2^11
+    // y_temp_1 = (y_temp_1 + 1) >> 2; // scale factor back to 2^11
 
     *x = x_temp_1;
     *y = y_temp_1;
