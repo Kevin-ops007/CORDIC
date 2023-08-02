3af419f6a8df14e404565ab3ca6b8bf8f7fd730a
Jack Klobchar <jackklobchar@gmail.com>
2023-08-01 19:48:31 -0700

    fix error

--- cordic_V_neon.c
+++ cordic_V_neon.c
@@ -7,24 +7,24 @@ void cordic_V_fixed_point(int *x, int *y, int *z)
     int i;
 
     // int16_t are 16 bit integers
-    int16x2_t xy = {*x, *y};
+    int16x4_t xy = {*x, *y};
     xy = vshl_n_s16(xy, 2);
 
     int16_t sign = 0;
     int16_t z_temp = 0;
 
     // 2 element vector to hold x and y
-    int16x2_t yx;
+    int16x4_t yx;
 
     for (i = 0; i < 11; i++)
     {
         yx = vrev64_s16(xy);
         /* 11 iterations are needed */
         sign = vget_lane_s16(xy, 1) > 0 ? 1 : -1;
-        int16x2_t sign_vec = {sign, -sign};
+        int16x4_t sign_vec = {sign, -sign};
 
         // right shift by i
-        int16x2_t shift = {-i, -i};
+        int16x4_t shift = {-i, -i};
         yx = vshl_s16(yx, shift);
 
         // multiply and accumulate
