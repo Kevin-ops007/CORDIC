a1e9247a3a4cbe9227ee263616d7ecfea3d8cd73
kevin <kmathew@peloton-technologies.com>
2023-08-03 11:47:18 -0700

    trying to fix arm code

--- cordic_V_inline_arm.c
+++ cordic_V_inline_arm.c
@@ -14,10 +14,10 @@ void cordic_V_fixed_point(int *x, int *y, int *z)
     {
         /* 11 iterations are needed */
         sign = y_temp_1 > 0 ? 1 : -1;
-        __asm [volatile] ("MLA %[output_i], %[input_i], %[input_j]"
+        __asm ("MLA %[output_i], %[input_i], %[input_j]"
         :[output_i] "r" (x_temp_2): [input_i] "r" (sign), [input_j] "r" (y_temp_1 >> i)
         );
-        __asm [volatile] ("MLS %[output_i], %[input_i], %[input_j]"
+        __asm ("MLS %[output_i], %[input_i], %[input_j]"
         :[output_i] "r" (y_temp_2): [input_i] "r" (sign), [input_j] "r" (x_temp_1 >> i)
         );
         z_temp += sign * z_table[i];
