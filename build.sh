ead9d34dbbe54279cdd08269d6037ca1177d7817
kevin <kmathew@peloton-technologies.com>
2023-08-03 22:50:00 -0700

    changing build script

--- build.sh
+++ build.sh
@@ -6,21 +6,21 @@ main_file="cordic_timing.c"
 # Get a list of all C files with the pattern cordic_V_*.c in the current directory
 c_files=(cordic_V_*.c)
 
-gcc -O1 cordic_timing.c cordic_V_standard.c -o cordic_O1.exe -lm
-gcc -O2 cordic_timing.c cordic_V_standard.c -o cordic_O2.exe -lm
+gcc -O1 cordic_timing.c cordic_V_standard.c -o cordic_O1_ex -lm
+gcc -O2 cordic_timing.c cordic_V_standard.c -o cordic_O2_ex -lm
 
 for c_file in "${c_files[@]}"; do
     # Extract the part after "cordic_V_" from the C file name
     name=${c_file#cordic_V_}
     name=${name%.c}
-    output_executable="cordic_$name.exe"
+    output_executable="cordic_$name"+"_ex"
     arm_code="cordic_$name.s"
 
     # Skip cordic_V_32bit.c
     if [[ $c_file == "cordic_V_32bit.c" ]]; then
         # Generate the output executable name
         echo "COMPILING 32bit $c_file"
-        gcc -O3 cordic_main_32bit_no_io.c cordic_V_32bit.c -o cordic_32bit.exe -lm
+        gcc -O3 cordic_main_32bit_no_io.c cordic_V_32bit.c -o cordic_32bit_ex -lm
         gcc -O3 -S "$c_file" -o "$arm_code"
     elif [[ $c_file == "cordic_V_neon.c" ]]; then
         echo "SKIPPING NEON FOR NOW"
@@ -37,11 +37,11 @@ for c_file in "${c_files[@]}"; do
 done
 
 rm -f ./assembly/*.s 
-rm -f ./executables/*.exe
+rm -f ./executables/*_ex
 rm -f ./executables/*.txt
 
 mv *.s ./assembly
-mv *.exe ./executables
+mv *_ex ./executables
 
 cd executables
 
