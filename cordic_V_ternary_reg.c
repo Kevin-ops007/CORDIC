9e2f9e6b85133c75c1af4c6c0d06d1a37cfbdd55
Jack Klobchar <jackklobchar@gmail.com>
2023-08-03 20:56:10 -0700

    try additional ternary optimizations

---
+++ cordic_V_ternary_reg.c
@@ -0,0 +1,32 @@
+int z_table[11];
+
+void cordic_V_fixed_point(int *x, int *y, int *z)
+{
+    register int x_temp_1, y_temp_1, z_temp;
+    register int x_temp_2;
+    register int i;
+    x_temp_1 = (*x) << 2;
+    y_temp_1 = (*y) << 2;
+    z_temp = 0;
+    register int sign = 0;
+
+    for (i = 0; i < 11; i++)
+    {
+        /* 11 iterations are needed */
+        sign = y_temp_1 > 0 ? 1 : -1;
+
+        x_temp_2 = x_temp_1 + (sign * (y_temp_1 >> i));
+        y_temp_1 = y_temp_1 - (sign * (x_temp_1 >> i));
+        z_temp += sign * z_table[i];
+
+        x_temp_1 = x_temp_2;
+    }
+
+    x_temp_1 *= 1244;
+    x_temp_1 = (x_temp_1 + 1) >> 13; // scale factor back to 2^11
+    y_temp_1 = (y_temp_1 + 1) >> 2;  // scale factor back to 2^11
+
+    *x = x_temp_1;
+    *y = y_temp_1;
+    *z = z_temp;
+}
